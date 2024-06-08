5eeb1ff48ff5acd5f8b81c76316ae9b1cea8a1d1
FahrulRR <a710230063@student.ums.ac.id>
2024-06-08 14:57:45 +0700

    Create day4.py
    operasi pada string

---
+++ Day 4/day4.py
@@ -0,0 +1,9 @@
+string = "Halo, nama saya fahrul"
+
+print("panjang string = ", len(string))
+print("index pertama huruf f berada di ", string.index("f"))
+print("huruf a muncul sebanyak ", string.count("a"))
+print(string.upper())
+print(string.lower())
+print(string.split(" "))
+
